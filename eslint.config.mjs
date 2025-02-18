22c0ab1b5ace79c0e621cd6da7e98af910dbe9bc
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-02-18 03:30:58 +0000

    tooling: Update eslint config for new version.

---
+++ eslint.config.mjs
@@ -0,0 +1,71 @@
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+), {
+    ignores: [
+        "dist/**",
+        "dist/src/**",
+        "postcss.config.js",
+        "tailwind.config.js",
+        "stylelint.config.js",
+    ],
+},{
+    plugins: {
+        react,
+        "@typescript-eslint": typescriptEslint,
+    },
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            ecmaFeatures: {
+                jsx: true,
+            },
+        },
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+    
+    rules: {
+        "react/prop-types": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
+        }],
+    },
+    
+}];
