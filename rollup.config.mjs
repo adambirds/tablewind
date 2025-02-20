0ce19b2f9170bf8ee5bc85fead383499b40ad773
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-02-20 14:00:00 +0000

    build: Add css file to build.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,17 +2,24 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+
 
 export default {
   input: {
     react: 'src/react.tsx',
-    next: 'src/next.tsx'
+    next: 'src/next.tsx',
   },
   external: ['react', 'react-dom', '@headlessui/react'],
   plugins: [
     nodeResolve(),
     commonjs(),
-    typescript({ tsconfig: './tsconfig.build.json' })
+    typescript({ tsconfig: './tsconfig.build.json' }),
+    copy({
+      targets: [
+        { src: 'src/tablewind.css', dest: 'dist' }
+      ]
+    })
   ],
   output: [
     {
