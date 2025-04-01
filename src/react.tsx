c6a8d0a3ff22c456976933fbb5ba3efdc4c38f1b
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-04-01 04:48:45 +0100

    library: Pass navigate function to datatable as a prop.

--- src/react.tsx
+++ src/react.tsx
@@ -1,15 +1,21 @@
 // src/react.tsx
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { NavigateFunction } from 'react-router-dom';
 import DataTableBase, { DataTableComponentProps } from './components/DataTable';
 
-export function DataTable<T extends { id: string }>(
-    props: DataTableComponentProps<T>
-): React.ReactElement {
-    const navigateHook = useNavigate();
-    const reactNavigate = (url: string) => {
-        navigateHook(url);
-    };
+export interface DataTablePropsWithRouter<T extends { id: string }>
+  extends DataTableComponentProps<T> {
+  navigate: NavigateFunction; // Expect the navigate function to be passed in as a prop
+}
 
-    return <DataTableBase {...props} navigate={reactNavigate} />;
+export function DataTable<T extends { id: string }>({
+  navigate,
+  ...props
+}: DataTablePropsWithRouter<T>): React.ReactElement {
+  // Use the passed-in navigate function
+  const reactNavigate = (url: string) => {
+    navigate(url);
+  };
+
+  return <DataTableBase {...props} navigate={reactNavigate} />;
 }
